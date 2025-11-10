a1e28313bc57596dd2b0147aa9869f75771ae36d
Ahmad <ahmadalkali.dev@gmail.com>
2025-11-10 10:48:13 +0100

    Red Test

---
+++ src/test/java/PrimeCheckerTest.java
@@ -0,0 +1,12 @@
+import org.example.PrimeChecker;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class PrimeCheckerTest {
+    private final PrimeChecker primeChecker = new PrimeChecker();
+
+    @Test
+    public void test0IsNotPrime() {
+        assertFalse(primeChecker.isPrime(0), "0 should not be considered a prime number");
+    }
+}
