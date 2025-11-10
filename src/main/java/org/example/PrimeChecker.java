e30399059dbde05b44b222e24253aa722115f506
iiriskivela <98057737+iiriskivela@users.noreply.github.com>
2025-11-10 11:17:28 +0100

    commit5

--- src/main/java/org/example/PrimeChecker.java
+++ src/main/java/org/example/PrimeChecker.java
@@ -3,7 +3,7 @@ package org.example;
 public class PrimeChecker {
     public boolean isPrime (int number) {
         if (number == 1) {
-            false;
+            return false;
         }
         return false;
     }
