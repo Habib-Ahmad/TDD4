294fd30137920c41cbf540dec440f38e436bc098
iiriskivela <98057737+iiriskivela@users.noreply.github.com>
2025-11-10 11:17:06 +0100

    commit4

--- src/main/java/org/example/PrimeChecker.java
+++ src/main/java/org/example/PrimeChecker.java
@@ -3,7 +3,7 @@ package org.example;
 public class PrimeChecker {
     public boolean isPrime (int number) {
         if (number == 1) {
-            throw new UnsupportedOperationException("Not implemented yet");
+            false;
         }
         return false;
     }
