4f41d1418e4509ce72a1a91e2f166eeda891bc63
Ahmad <ahmadalkali.dev@gmail.com>
2025-11-10 11:14:05 +0100

    Red test for prime number 1

--- src/main/java/org/example/PrimeChecker.java
+++ src/main/java/org/example/PrimeChecker.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class PrimeChecker {
     public boolean isPrime (int number) {
+        if (number == 1) {
+            throw new UnsupportedOperationException("Not implemented yet");
+        }
         return false;
     }
 }
--- src/test/java/PrimeCheckerTest.java
+++ src/test/java/PrimeCheckerTest.java
@@ -9,4 +9,9 @@ public class PrimeCheckerTest {
     public void test0IsNotPrime() {
         assertFalse(primeChecker.isPrime(0), "0 should not be considered a prime number");
     }
+
+    @Test
+    public void test1IsNOtPrime() {
+        assertFalse(primeChecker.isPrime(1), "1 should not be considered a prime number");
+    }
 }
